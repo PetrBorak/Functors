29547050a360aef1079bf28cccf0545a2404c24f
Petr Borak <petr.borak@moneta.cz>
2020-04-13 17:21:41 +0200

    [PBO] - fix missing Task export

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ export * from './src/Either/index';
 export * from './src/createCompose/index';
 export * from './src/Map/index';
 export * from './src/Maybe/index';
+export * from './src/Task/index';
 
